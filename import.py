361dd1cd71011d8af78b69b2b0d049e4d9a4a185
ArtemProc <10011238+ArtemProc@users.noreply.github.com>
2021-01-04 14:13:18 +0100

    Update import.py

--- import.py
+++ import.py
@@ -25,12 +25,9 @@
     Please be aware that all public code samples provided by Artem are unofficial in nature, are provided as examples only, 
     are unsupported and will need to be heavily scrutinized and potentially modified before they can be used in a production environment. All such code samples are provided on an as-is basis, and Artem expressly disclaims all warranties, express or implied.
   
-<<<<<<< HEAD
-    All code samples are © Nutanix, Inc., and are provided as-is under the MIT license. (https://opensource.org/licenses/MIT)
-=======
     All code samples are provided as-is under the MIT license. (https://opensource.org/licenses/MIT)
 
->>>>>>> e5d9d41069b639b64d41a04f717bfeb73b9b9c0e
+
 '''
 import sys
 import os
